4f17a6688bf4b4e30352b4cbf1c6a3ed4d425e2c
Christopher1120 <christophercarlpogi20@gmail.com>
2023-11-28 07:13:01 +0800

    Updated

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var { app, BrowserWindow, ipcMain, dialog } = require("electron");
+var { autoUpdater } = require("electron-updater");
 var path = require("node:path");
+const log = require("electron-log");
 
 let server = require("./server");
 
+log.transports.file.resolvePath = () => path.join(__dirname, "logs")
 
 function AppUpdate() {
     const options = {
@@ -36,9 +39,27 @@ app.whenReady().then(() => {
         if (BrowserWindow.getAllWindows().length === 0) {
             ipcMain.handle('ping', () => 'pong')
             createWindow();
-
+            autoUpdater.checkForUpdatesAndNotify();
         }
     })
+})
+
+
+
+autoUpdater.on("update-available", () => {
+    log.info("update-vailable");
+})
+
+autoautoUpdater.on("checking-for-updates", () => {
+    log.info("checking-for-updates");
+})
+
+autoautoUpdater.on("download-progress", () => {
+    log.info("download-progress");
+})
+
+autoautoUpdater.on("update-downloaded", () => {
+    log.info("update-downloaded");
 })
 
 
